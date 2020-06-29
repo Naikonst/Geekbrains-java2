da24d7056b8f127216edb0e6f906d82b5aa0a19a
Igor Balandin <ibalandin@unitarius.ru>
2020-06-29 18:58:08 +0300

    Первый урок - Интерфейсы

---
+++ lesson1/src/main/java/ru/geekbrains/Telegram.java
@@ -0,0 +1,19 @@
+package ru.geekbrains;
+
+public class Telegram extends Application implements Messenger {
+
+    public Telegram() {
+        this.name = "Telegram";
+        this.version = "1.0";
+    }
+
+    @Override
+    public String prepareMessage() {
+     return "";
+    }
+
+    @Override
+    public void processMessage() {
+
+    }
+}
